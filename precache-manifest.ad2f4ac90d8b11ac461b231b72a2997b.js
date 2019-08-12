c97314b53c8b5b9f1c8755c786e2bb332fd2baed
nidvar <djayshine87@hotmail.com>
2019-08-12 23:29:04 +1000

    Updates

---
+++ precache-manifest.ad2f4ac90d8b11ac461b231b72a2997b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b1e0adfbd06989b7cfe5ad37711292",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "1da628ed7d6365bf6adc",
+    "url": "/pokemon/static/css/main.2f8fb465.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/pokemon/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1da628ed7d6365bf6adc",
+    "url": "/pokemon/static/js/main.bbe706dc.chunk.js"
+  },
+  {
+    "revision": "b04917ec47d1c084d1e9",
+    "url": "/pokemon/static/js/runtime~main.0909cbae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemon/static/media/logo.5d5d9eef.svg"
+  }
+]);
